0a1f5d0a03e853d22556c7c4d4f1d9c8537b5273
Yan Andryshkin <yan_Andryshkin@epam.com>
2021-04-07 12:20:56 +0300

    fix: add cors headers to lambdas

--- product-service/hadlers/getProductsList.js
+++ product-service/hadlers/getProductsList.js
@@ -2,6 +2,11 @@ const data = require('../mock');
 
 module.exports.getProductsList = async (event) => {
   return {
+    headers: {
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET'
+    },
     statusCode: 200,
     body: JSON.stringify(data)
   };
