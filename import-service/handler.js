37f89f8addb915573437b0aa0807e137cfb1041d
Yan Andryshkin <yan_Andryshkin@epam.com>
2021-05-23 12:13:41 +0300

    feat: configure catalagoBatchProcess lambda

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,7 +1,9 @@
 const { importProductsFile } = require('./handlers/importProductsFile');
 const { importFileParser } = require('./handlers/importFileParser');
+const { catalogBatchProcess } = require('./handlers/catalogBatchProcess');
 
 module.exports = {
   importProductsFile,
-  importFileParser
+  importFileParser,
+  catalogBatchProcess
 };
--- import-service/handlers/catalogBatchProcess.js
+++ import-service/handlers/catalogBatchProcess.js
@@ -1,27 +1,46 @@
-const { SQS } = require('aws-sdk');
+const { Client } = require('pg');
 const middy = require('middy');
 const { cors } = require('middy/middlewares');
 
-const Bucket = 'aws-task-csv-bucket';
+const { host, port, database, user, password } = process.env;
 
-const handler = async (event, ctx, cb) => {
-  const sqs = new SQS();
-  const files = JSON.parse(event.body);
+const dbOptions = {
+  host,
+  port,
+  database,
+  user,
+  password,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+};
 
-  files.forEach((file) => {
-    sqs.sendMessage(
-      {
-        QueueUrl: process.env.SQS_URL,
-        MessageBody: file
-      },
-      () => {
-        console.log('file sended');
-      }
+const selectAll = (title, description, price) =>
+  `select * from products where title='${title}' and description='${description}' and price='${price}'`;
+
+const handler = async (event) => {
+  const client = new Client(dbOptions);
+  await client.connect();
+
+  const productsList = event.Records.map(({ body }) => body);
+
+  productsList.forEach(async (product) => {
+    const { title, description, price, image } = JSON.parse(product);
+
+    await client.query(
+      `insert into products (title, description, price, image) values
+      ('${title}', '${description}', ${price}, '${image || ''}')`
     );
-  });
 
-  cb(null, {
-    statusCode: 200
+    const {
+      rows: [{ id }]
+    } = await client.query(selectAll(title, description, price));
+
+    await client.query(
+      `insert into stocks (product_id, count) values
+      ('${id}', '1')`
+    );
   });
 };
 
